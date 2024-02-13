1286efbb535ab14d69190da4bd24106c76b188d3
YYG609 <yyg864@gmail.com>
2024-02-13 15:00:45 +0900

    fix: ã조회수, 게시일 포맷 변경

--- app/src/main/java/com/example/bottomnavi/mypagefragment/MypageAdapter.kt
+++ app/src/main/java/com/example/bottomnavi/mypagefragment/MypageAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.bottomnavi.mypagefragment
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -12,6 +14,8 @@ import com.example.bottomnavi.R
 import com.example.bottomnavi.databinding.FragmentMypageItemRecyclerviewBinding
 import com.example.bottomnavi.homefragment.HomeFragment.Companion.likeList
 import com.example.bottomnavi.homefragment.MyVideo
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 class MypageAdapter(private var mItems: MutableList<MyVideo.MyVideoItems>) :
     RecyclerView.Adapter<MypageAdapter.MyVideoViewHolder>() {
@@ -39,14 +43,19 @@ class MypageAdapter(private var mItems: MutableList<MyVideo.MyVideoItems>) :
         return MyVideoViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyVideoViewHolder, position: Int) {
         holder.title.text = mItems[position].title
         Glide.with(holder.itemView.context)
             .load(mItems[position].thumbnail)
             .into(holder.thumbnails)
-        holder.views.text = mItems[position].views.toString()
+        // 조회수 포맷
+        holder.views.text = "%,d".format(mItems[position].views?.toLong())+"회"
         holder.uploader.text = mItems[position].channelTitle
-        holder.datetime.text = mItems[position].publishedAt
+        // 게시일 포맷
+        val parsed = OffsetDateTime.parse(mItems[position].publishedAt)
+        val formatter = parsed.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        holder.datetime.text = formatter
 
         holder.items.setOnClickListener {
             val myData = MyVideo.MyVideoItems(
